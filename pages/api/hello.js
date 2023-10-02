9b3323ec6a6536e8e78a527fa6cecdaee1e64bf0
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-10-02 11:22:41 +0200

    testing

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,8 +3,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
 
 
