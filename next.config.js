37ee04aed90b1202275ab0b075c39d61be058811
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-10-02 11:48:14 +0200

    creating a netlify serverless function

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // ESLint is disabled for experimental use.
-  // eslint: false,
-  webpack: {
-    resolve: {
-      alias: {
-        '/env': './.env',
-      },
-    },
-  },
 };
 
 module.exports = nextConfig;
