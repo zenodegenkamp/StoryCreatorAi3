794f63ed59c157caf4352657faee83c5ab766b38
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-10-02 13:14:30 +0200

    Serverless function is created

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,36 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import OpenAI from 'openai';
-
-
-console.log(process.env.OPENAI_API_KEY);
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
-});
 
 
 export async function fetchBotReply(mainCharacter) {
 
-    const response = await openai.completions.create({
-        model: 'text-davinci-003',
-        prompt: `Generate a short twitter like message to enthusiastically say that you are hyped to create a story for the main character that is given by ${mainCharacter}
-        ###
-        main character: Zeno
-        message: Hi Zeno, I'm going to write a super exciting story about you! Where you will discover new things and do cool stuff. Let's create your story!
-        ###
-        main character: Max
-        message: Hey Max! I'm very excited to create a story about you! In this adventure, you will travel to unique places and have a lot of fun.
-        ###
-        main character: Patrick
-        message: Message: Hi Patrick, I'm super excited to write a story about you! You're going on an adventure and experiencing all sorts of fun things. Let's bring your story to life!
-        ###
-        main character: ${mainCharacter}
-        message: 
-        `,
-        max_tokens: 100
-      })
-      return response.choices[0].text
+    const url = 'https://marvelous-fenglisu-f6d29b.netlify.app/.netlify/functions/fetchAI'
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'text/plain',
+      },
+      body: mainCharacter
+    })
+    const data = await response.json()
+    console.log("APIFILE" + data)
+  
+    return data
+
+    // const response = await openai.completions.create({
+    //     model: 'text-davinci-003',
+    //     prompt: `Generate a short twitter like message to enthusiastically say that you are hyped to create a story for the main character that is given by ${mainCharacter}
+    //     ###
+    //     main character: Zeno
+    //     message: Hi Zeno, I'm going to write a super exciting story about you! Where you will discover new things and do cool stuff. Let's create your story!
+    //     ###
+    //     main character: Max
+    //     message: Hey Max! I'm very excited to create a story about you! In this adventure, you will travel to unique places and have a lot of fun.
+    //     ###
+    //     main character: Patrick
+    //     message: Message: Hi Patrick, I'm super excited to write a story about you! You're going on an adventure and experiencing all sorts of fun things. Let's bring your story to life!
+    //     ###
+    //     main character: ${mainCharacter}
+    //     message: 
+    //     `,
+    //     max_tokens: 100
+    //   })
+    //   return response.choices[0].text
 
 }
 
