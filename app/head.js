23253cc4ace3db1c13941d3061372304c787b5f5
zenode <Zenodegenkamp@hotmail.com>
2023-12-12 12:00:05 +0100

    Update head.js

--- app/head.js
+++ app/head.js
@@ -2,6 +2,7 @@ const Head = () => (
   <>
     <title>Story Creator AI</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <meta name="google-site-verification" content="oXhCfod6_H8S2JK93Xba3FbvAW7ti0vYbls04Dj3fSU" />
     <link rel="icon" href="/favicon.ico" />
     
   </>
