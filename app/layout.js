887e3aca20b58dd6cbc1824b2623d0207b260ca8
zenode <Zenodegenkamp@hotmail.com>
2023-12-12 12:01:20 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ const RootLayout = ({ children }) => (
       <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,900&display=swap" rel="stylesheet" />
-
+          <meta name="google-site-verification" content="oXhCfod6_H8S2JK93Xba3FbvAW7ti0vYbls04Dj3fSU" />
         </head>
         <body>
           {children}
